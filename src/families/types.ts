6b6672f1529d1ae1c08b534d37afc2aa8f4175b8
Elias Estrada <elicharles.e@gmail.com>
2023-01-05 20:06:06 -0800

    Initialize add-missing-tests

---
+++ src/families/types.ts
@@ -0,0 +1,16 @@
+/**
+ * Supported coin families
+ */
+enum FAMILIES {
+  BITCOIN = "bitcoin",
+  ETHEREUM = "ethereum",
+  ALGORAND = "algorand",
+  CRYPTO_ORG = "crypto_org",
+  RIPPLE = "ripple",
+  COSMOS = "cosmos",
+  TEZOS = "tezos",
+  POLKADOT = "polkadot",
+  STELLAR = "stellar",
+  TRON = "tron",
+}
+export default FAMILIES;
