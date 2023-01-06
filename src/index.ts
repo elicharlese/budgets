53e4517202a090144251e8ddbc246d610f6be523
Elias Estrada <elicharles.e@gmail.com>
2023-01-05 20:06:03 -0800

    Initialize LL-6366-integrate-in-ledger-live-platform-apps

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export * from "./types";
+export * from "./rawTypes";
+export * from "./families/ethereum/types";
+export * from "./families/bitcoin/types";
+export { default as FAMILIES } from "./families/types";
+
+export { default as Mock } from "./mock";
+
+export * from "./transports";
+
+export * from "./serializers";
+
+export { default } from "./LedgerLivePlatformSDK";
